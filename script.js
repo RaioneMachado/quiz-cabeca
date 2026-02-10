b49acadd0c0f9c5415ac03b57740796b4e996ad7
dev-anon <dev@users.noreply.github.com>
2026-02-10 17:13:07 -0300

    atualização

---
+++ script.js
@@ -0,0 +1,12 @@
+/**
+ * Função para processar a escolha do usuário
+ * @param {string} idade - A faixa etária selecionada
+ */
+function irParaProximaEtapa(idade) {
+    console.log("Usuário selecionou a faixa: " + idade);
+    
+    // Altere o link abaixo para a próxima página do seu funil
+    // Exemplo: window.location.href = "etapa2.html";
+    
+    alert("Iniciando plano personalizado de Cabeça do Drop para a faixa " + idade);
+}
